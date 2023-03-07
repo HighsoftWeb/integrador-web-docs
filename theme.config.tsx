5dc6000df95e9e2d986a6d857b5c044d0d720255
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2023-03-07 15:56:03 -0300

    feat: favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: (
+      <>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </>
+  ),
   logo: <span>Integrador Web Developers</span>,
   project: {
     link: 'https://github.com/HighsoftWeb/integrador-web-docs',
